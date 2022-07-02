72c66708052f74d8c4d4aacecc1005b10565d2a1
HosseinEnsafi <HosseinEnsafi@outlook.com>
2022-07-02 11:20:28 -0400

    another commit

---
+++ src/layouts/Admin/Navbar.js
@@ -0,0 +1,69 @@
+import React, { useContext, useEffect, useState } from "react";
+import { BiListUl, BiUser, BiMoon, BiSun } from "react-icons/bi";
+import { UIContext } from "../../context/UIState/UIContext";
+import LoginModal from "../../components/UI/LoginModal/LoginModal";
+import { useSelector } from "react-redux";
+import UserProfile from "../../components/UI/UserProfile";
+
+function Navbar() {
+  const {
+    currentTheme,
+    openLogin,
+    setOpenLogin,
+    setCurrentTheme,
+    setActiveMenu,
+  } = useContext(UIContext);
+
+  const { user } = useSelector((state) => state.auth);
+
+  const [initialLoad, setInitialLoad] = useState(true);
+
+  useEffect(() => {
+    if (initialLoad) {
+      setInitialLoad(false);
+      return;
+    }
+
+    localStorage.setItem("theme", currentTheme);
+  }, [currentTheme]);
+
+  return (
+    <header className="fixed top-0  left-0 flex h-[50px] w-full items-center justify-between bg-white p-2 shadow-md dark:bg-[#3d3d3d] md:z-10 md:pl-12">
+      <div className=" flex items-center gap-3">
+        <button
+          onClick={() => setActiveMenu((prevState) => !prevState)}
+          className="hoverAnimation h-10 w-10"
+        >
+          <BiListUl className="h-full w-full" />
+        </button>
+        <h2>لوگو شرکت</h2>
+      </div>
+      <div className="flex items-center gap-1">
+        <button
+          className="hoverAnimation flex h-10 w-10 items-center justify-center text-gray-500"
+          onClick={() => {
+            setCurrentTheme(currentTheme === "Dark" ? "Light" : "Dark");
+          }}
+        >
+          {currentTheme === "Dark" && <BiMoon className="h-full w-full" />}
+          {currentTheme === "Light" && <BiSun className="h-full w-full" />}
+        </button>
+
+        {!user ? (
+          <div className="hoverAnimation flex h-10 w-10 items-center text-gray-500">
+            <BiUser
+              onClick={() => setOpenLogin((prevState) => !prevState)}
+              className="h-full w-full"
+            />
+          </div>
+        ) : (
+          <UserProfile />
+        )}
+      </div>
+
+      {openLogin && !user && <LoginModal />}
+    </header>
+  );
+}
+
+export default Navbar;
---
+++ src/layouts/Admin/index.js
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect } from "react";
+import { UIContext } from "../../context/UIState/UIContext";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+function Layout({ children }) {
+  const { currentTheme, setCurrentTheme, screenSize, activeMenu } =
+    useContext(UIContext);
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem("theme");
+    if (currentThemeColor) setCurrentTheme(currentThemeColor);
+  }, []);
+
+  return (
+    <div className={`${currentTheme === "Dark" ? "dark" : ""} `}>
+      <div className=" text-neutral-800 dark:bg-neutral-800 dark:text-slate-50">
+        <Navbar />
+        <Sidebar />
+
+        <main
+          className={`min-h-screen  mx-auto${
+            screenSize > 900 && activeMenu ? " mr-[270px]" : "mr-0"
+          } max-w-7xl flex-grow bg-white px-5 pt-16 dark:bg-neutral-800 md:px-8 `}
+        >
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
