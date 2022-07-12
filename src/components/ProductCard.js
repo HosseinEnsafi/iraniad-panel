e25f288c7a82875971fa8d335b87a0b32bed8658
HosseinEnsafi <HosseinEnsafi@outlook.com>
2022-07-12 09:35:48 -0400

    modifying productCard and style it

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -25,21 +25,19 @@ function ProductCard({ item }) {
     }
 
     if (!currentPlan) {
-      setPrice("بازه مورد نظر یافت نشد");
+      setPrice("خارج از بازه");
       return;
     }
-    console.log(currentPlan);
 
     setPrice(amount * currentPlan.cost + " تومان ");
   }, [amount]);
 
   return (
     <article className="relative w-full max-w-md bg-gray-200  p-5 shadow transition-shadow hover:shadow-lg dark:bg-neutral-500">
-      <div className="pricing__label absolute left-2 -top-[5px] flex h-20 w-[4.375rem] items-center justify-center whitespace-nowrap bg-gradient-to-tl  from-blue-400 to-blue-500  text-[15px] text-white">
+      <div className="pricing__label absolute left-4 -top-[12px] flex h-20 w-[4.375rem] items-center justify-center whitespace-nowrap bg-gradient-to-tl  from-blue-400 to-blue-500  text-[15px] text-white">
         <span className="">{label}</span>
       </div>
       <div className="flex h-full flex-col">
-        {/* <h2 className=" mt-2 text-xl"></h2> */}
         <ul className=" pt-6 leading-10">
           {periods &&
             periods.map((plan, i) => {
@@ -93,48 +91,57 @@ function ProductCard({ item }) {
             })}
         </ul>
 
-        <form className="mt-auto flex  gap-2 py-4">
-          <div className="flex  items-center rounded-lg border-2 border-gray-500 border-opacity-50 dark:border-gray-300">
-            <span
-              onClick={() => setAmount((prevAmount) => prevAmount + amountStep)}
-              className="flex h-full w-8  cursor-pointer select-none items-center  justify-center rounded-tr-md rounded-br-md bg-[#ccc] bg-opacity-80 dark:bg-slate-900"
-            >
-              {" "}
-              +{" "}
-            </span>
-            <input
-              dir="ltr"
-              className=" hide-arrow h-full w-20 p-2 outline-none dark:bg-slate-700"
-              type="number"
-              name="amount"
-              id="amount"
-              step={100}
-              min={0}
-              value={amount}
-              onChange={(e) => setAmount(+e.target.value)}
-            />
-            <span
-              onClick={() =>
-                setAmount((prevAmount) => {
-                  if (amount < amountStep) return prevAmount;
-                  return prevAmount - amountStep;
-                })
-              }
-              className="flex h-full w-8 cursor-pointer select-none items-center  justify-center rounded-tl-md rounded-bl-md bg-[#ccc]  bg-opacity-80  dark:bg-slate-900"
-            >
-              {" "}
-              -{" "}
-            </span>
+        <form className="mt-auto pt-4" onSubmit={(e) => e.preventDefault()}>
+          <div className="flex gap-2">
+            <div className="flex  max-h-12 rounded-lg border-2 border-gray-500 border-opacity-50 dark:border-gray-300">
+              <span
+                onClick={() =>
+                  setAmount((prevAmount) => prevAmount + amountStep)
+                }
+                className="flex h-full w-8  cursor-pointer select-none items-center  justify-center rounded-tr-md rounded-br-md bg-[#ccc] bg-opacity-80 dark:bg-slate-900"
+              >
+                {" "}
+                +{" "}
+              </span>
+              <input
+                dir="ltr"
+                className=" hide-arrow  w-20 p-2 outline-none dark:bg-slate-700"
+                type="number"
+                name="amount"
+                id="amount"
+                step={100}
+                min={0}
+                value={amount}
+                onChange={(e) => setAmount(+e.target.value)}
+              />
+              <span
+                onClick={() =>
+                  setAmount((prevAmount) => {
+                    if (amount < amountStep) return prevAmount;
+                    return prevAmount - amountStep;
+                  })
+                }
+                className="flex h-full w-8 cursor-pointer select-none items-center  justify-center rounded-tl-md rounded-bl-md bg-[#ccc]  bg-opacity-80  dark:bg-slate-900"
+              >
+                {" "}
+                -{" "}
+              </span>
+            </div>
+            {amount > maxOrder && (
+              <p className="self-center text-sm text-red-500  dark:text-white md:text-base">
+                نمیتوانید بیش از {toK(maxOrder)} انتخاب کنید
+              </p>
+            )}
+            ‍
+            {amount < maxOrder && (
+              <p className=" mr-2 self-center  text-sm md:text-base">
+                {price}{" "}
+              </p>
+            )}
           </div>
-
-          {amount > maxOrder && (
-            <p className="self-center text-[14px] text-red-500 dark:text-white">
-              شما نمیتوانید بیشتر از {toK(maxOrder)} انتخاب کنید
-            </p>
-          )}
-          {amount < maxOrder && (
-            <p className=" self-center text-lg">{price} </p>
-          )}
+          <button className="mt-6 rounded-2xl bg-gradient-to-tr  from-blue-400 to-blue-500 p-3 text-white ">
+            ثبت سفارش
+          </button>
         </form>
       </div>
     </article>
